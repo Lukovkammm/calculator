961809cb4c16d2bd13936cc277c838809a60234a
Marina <artyusmarina@yandex.ru>
2021-01-14 15:16:19 +0300

    refactor: change structure

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const display = document.querySelector('#display');
+
 let hiddingOperator = false;
 let currentMemory = '';
 let calculate = false;
@@ -6,84 +7,75 @@ let prevOperator = '';
 let onlyNumber = false;
 let usedDecimal = false;
 
-const pressNumber = () => {
-    const numbers = document.querySelectorAll('.number');
-    numbers.forEach(number => {
-        number.addEventListener('click', (e) => {
-            let number = e.target.textContent;
-            if (display.value.length > 12 && prevOperator === '' || usedDecimal && number === '.') {
-                return false;
-            }
-            if (display.value === '0' || hiddingOperator) {
-                if (number === '.') {
-                    display.value += number;
-                    usedDecimal = true;
-                } else {
-                    display.value = number;
-                }
-                hiddingOperator = false;
-            } else {
-                display.value += number;
-                if (number === '.') {
-                    usedDecimal = true;
-                }
-            }
-            onlyNumber = false;
-        });
-    });
-}
-
-const pressOperation = () => {
-    const operations = document.querySelectorAll('.operator');
-
-    operations.forEach(operator => {
-        operator.addEventListener('click', (e) => {
-            let operator = e.target.textContent;
-            hiddingOperator = true;
-
-            if (!onlyNumber && prevOperator !== '=') {
-                if (!calculate && operator !== '=') {
-                    calculate = true;
-                    currentMemory = display.value;
-                } else {
-                    if (prevOperator !== '' && prevOperator !== '=') {
-                        currentMemory += prevOperator;
-                    }
-
-                    currentMemory += display.value;
-                    currentMemory = (parseFloat(eval(currentMemory).toFixed(12)));
-                    if (currentMemory === Infinity) {
-                        currentMemory = 0;
-                    }
-                    display.value = currentMemory;
-                }
-                onlyNumber = true;
-            }
-            prevOperator = operator;
-            usedDecimal = false;
-        });
-    });
-}
-
-const pressClearButton = () => {
-    const clearButtons = document.querySelectorAll('.clear');
-    clearButtons.forEach(clear => {
-        clear.addEventListener('click', (e) => {
-            let clearer = e.target.textContent;
-            usedDecimal = false;
-            display.value = '';
-
-            if (clearer === 'c' || clearer === 'ce' && prevOperator === '=') {
-                pendingOperator = false;
-                currentMemory = '';
-                calculate = false;
-                prevOperator = '';
-                onlyNumber = false;
-            }
-        })
+const init = () => {
+    const calcTable = document.querySelector('.calc-table');
+    calcTable.addEventListener('click', (e) => {
+        const buttonValue = e.target.textContent;
+        if (e.target.classList.contains('number')) {
+            onClickNumber(buttonValue);
+        } else if (e.target.classList.contains('operator')) {
+            onClickOperator(buttonValue);
+        } else if (e.target.classList.contains('clear')) {
+            onClickClear(buttonValue);
+        }
     })
 }
 
-pressNumber();
-pressOperation();
-pressClearButton();
+function onClickNumber(buttonValue) {
+    if (display.value.length > 12 && prevOperator === '' || usedDecimal && buttonValue === '.') {
+        return false;
+    }
+    if (display.value === '0' || hiddingOperator) {
+        if (buttonValue === '.') {
+            display.value += buttonValue;
+            usedDecimal = true;
+        } else {
+            display.value = buttonValue;
+        }
+        hiddingOperator = false;
+    } else {
+        display.value += buttonValue;
+        if (buttonValue === '.') {
+            usedDecimal = true;
+        }
+    }
+    onlyNumber = false;
+}
+
+function onClickOperator(buttonValue) {
+    hiddingOperator = true;
+
+    if (!onlyNumber && prevOperator !== '=') {
+        if (!calculate && buttonValue !== '=') {
+            calculate = true;
+            currentMemory = display.value;
+        } else {
+            if (prevOperator !== '' && prevOperator !== '=') {
+                currentMemory += prevOperator;
+            }
+            currentMemory += display.value;
+            currentMemory = (parseFloat(eval(currentMemory).toFixed(12)));
+            if (currentMemory === Infinity) {
+                currentMemory = 0;
+            }
+            display.value = currentMemory;
+        }
+        onlyNumber = true;
+    }
+    prevOperator = buttonValue;
+    usedDecimal = false;
+}
+
+function onClickClear(buttonValue) {
+    usedDecimal = false;
+    display.value = '';
+    if (buttonValue === 'c' || buttonValue === 'ce' && prevOperator === '=') {
+        pendingOperator = false;
+        currentMemory = '';
+        calculate = false;
+        prevOperator = '';
+        onlyNumber = false;
+    }
+}
+
+init();
